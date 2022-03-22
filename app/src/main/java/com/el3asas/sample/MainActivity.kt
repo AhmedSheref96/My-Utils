2ce54126730933dff218fe11d594d023e732cca9
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-03-22 17:00:21 +0200

    add utils library classes

--- app/src/main/java/com/el3asas/sample/MainActivity.kt
+++ app/src/main/java/com/el3asas/sample/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.btn)
+
         button.setOnClickListener {
             /*
             * ensure that override of resources
