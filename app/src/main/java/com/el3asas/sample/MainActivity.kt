a122db9146d92ac800df198a19671faf0368ac5d
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-03-30 16:46:40 +0200

    add utils library classes

--- app/src/main/java/com/el3asas/sample/MainActivity.kt
+++ app/src/main/java/com/el3asas/sample/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
             * primaryColor defined in app is red
             * */
             customSnackBar(it, "", com.el3asas.utils.R.drawable.ic_outline_error_outline_24) {}
-
         }
 
     }
