62ccce88ea7a4888dbbc79a4d3854b291c20907a
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-04-28 11:04:59 +0200

    add CirclePagerIndicatorDecoration.kt classes

--- Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
+++ Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
@@ -1,12 +1,8 @@
 package com.el3asas.utils.base
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.MutableStateFlow
 
 open class BaseViewModel : ViewModel() {
-    val isLoading = MutableLiveData(true)
-    val isError = MutableLiveData(false)
-
+    val isLoading = MutableStateFlow(true)
 }
