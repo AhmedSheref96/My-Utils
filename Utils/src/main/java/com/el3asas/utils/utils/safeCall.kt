87ff0942fc977e1704a0de147e1358e5c49bbf35
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-04-28 11:28:44 +0200

    add CirclePagerIndicatorDecoration.kt classes

--- Utils/src/main/java/com/el3asas/utils/utils/safeCall.kt
+++ Utils/src/main/java/com/el3asas/utils/utils/safeCall.kt
@@ -2,6 +2,7 @@ package com.el3asas.utils.utils
 
 suspend fun <T> safeCall(action: suspend () -> Response<T>): Response<T> {
     return try {
+
         action()
     } catch(e: Exception) {
         Response.Error(e.message ?: "An unknown error occurred")
