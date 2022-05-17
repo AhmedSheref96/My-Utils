ae32d6a09d5ffa99a3b481f984c36a43a0c1fda8
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-05-17 10:22:25 +0200

    edit getData

--- Utils/src/main/java/com/el3asas/utils/utils/dataCollectors.kt
+++ Utils/src/main/java/com/el3asas/utils/utils/dataCollectors.kt
@@ -1,6 +1,7 @@
 package com.el3asas.utils.utils
 
 import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class DataCollector<T>(
     private inline val onSuccess: ((T) -> Unit)? = null,
@@ -25,22 +26,23 @@ class DataCollector<T>(
 }
 
 fun <T> getData(
-    value: Response<T>, onSuccess: ((T) -> Unit)? = null,
+    value: Response<T>,
+    onSuccess: ((T) -> Unit)? = null,
     onError: ((String) -> Unit)? = null,
-    onLoading: ((Boolean) -> Unit)? = null
+    isLoading: MutableStateFlow<Boolean>? = null
 ) {
     when (value) {
         is Response.Success -> {
-            onLoading?.let { it(false) }
+            isLoading?.value = false
+
             value.data?.let { onSuccess?.let { it1 -> it1(it) } }
         }
         is Response.Error -> {
-            onLoading?.let { it(false) }
+            isLoading?.value = false
             value.message?.let { onError?.let { it1 -> it1(it) } }
         }
-        is Response.Loading -> {
-            onLoading?.let { it(true) }
+        else -> {
+            isLoading?.value = true
         }
     }
 }
-
