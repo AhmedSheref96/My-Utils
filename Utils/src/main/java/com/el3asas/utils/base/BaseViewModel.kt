0c984176565027e121bebda4d6f80bdc691fd104
Ahmed Sheref <53125308+ahmedsheef@users.noreply.github.com>
2022-05-24 13:46:48 +0200

    edit BaseViewModel.kt

--- Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
+++ Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
@@ -1,8 +1,13 @@
 package com.el3asas.utils.base
 
+import android.view.View
 import androidx.lifecycle.ViewModel
+import com.el3asas.utils.utils.clearNavigateStack
 import kotlinx.coroutines.flow.MutableStateFlow
 
 open class BaseViewModel : ViewModel() {
     val isLoading = MutableStateFlow(true)
+    fun onBack(v: View) {
+        clearNavigateStack(v, null)
+    }
 }
