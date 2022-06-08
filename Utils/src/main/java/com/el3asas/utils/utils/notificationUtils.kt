664f4e73fadcbaa725fbcfcd3a88b0623fcc2d41
Ahmed Sheref <ahmedsheef96@gmail.com>
2022-06-08 13:05:08 +0200

    edit notificationUtils.kt

--- Utils/src/main/java/com/el3asas/utils/utils/notificationUtils.kt
+++ Utils/src/main/java/com/el3asas/utils/utils/notificationUtils.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.el3asas.utils.R
 import java.util.*
@@ -22,11 +23,16 @@ fun NotificationManager.sendNotification(
     applicationContext: Context
 ) {
 
+    val flag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    } else {
+        PendingIntent.FLAG_UPDATE_CURRENT
+    }
     val contentPendingIntent = PendingIntent.getActivity(
         applicationContext,
         NOTIFICATION_ID,
         contentIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
+        flag
     )
 
     val eggImage = avatar?.let { getBitmapFromURL(it) }
