b11750ab4f20b8356259dcf0bb1697b2f53a76e5
Ahmed Sheref <ahmedsheef96@gmail.com>
2022-06-28 16:16:53 +0200

    [ADD ERROR STATES HANDLER TO VIEW MODEL]

--- Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
+++ Utils/src/main/java/com/el3asas/utils/base/BaseViewModel.kt
@@ -7,7 +7,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 open class BaseViewModel : ViewModel() {
     val isLoading = MutableStateFlow(true)
+    val isErrorOccur = MutableStateFlow(ErrorState(false))
+
     open fun onBack(v: View) {
         clearNavigateStack(v, null)
     }
 }
+
+data class ErrorState(val isError: Boolean, val message: String? = null)
