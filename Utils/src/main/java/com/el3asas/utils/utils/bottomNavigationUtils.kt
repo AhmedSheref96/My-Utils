073090f9e0b1cd8847a9e3cb83ee0b8524c8e2a1
Ahmed Sheref <ahmedsheef96@gmail.com>
2023-04-27 12:40:24 +0200

    [add bottomNavigationUtils]

---
+++ Utils/src/main/java/com/el3asas/utils/utils/bottomNavigationUtils.kt
@@ -0,0 +1,42 @@
+package com.el3asas.utils.utils
+
+import android.animation.Animator
+import android.view.View
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+private fun hideBottomNavigationView(
+    view: BottomAppBar, fab: FloatingActionButton
+) {
+    view.animate().translationY(view.height.toFloat())
+        .setListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {}
+            override fun onAnimationEnd(animation: Animator) {
+                view.visibility = View.GONE
+            }
+
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationRepeat(animation: Animator) {}
+        })
+    fab.hide()
+
+}
+
+private fun showBottomNavigationView(
+    view: BottomAppBar, fab: FloatingActionButton
+) {
+    view.animate().translationY(0f).setListener(object : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator) {
+            view.visibility = View.VISIBLE
+        }
+
+        override fun onAnimationEnd(animation: Animator) {}
+        override fun onAnimationCancel(animation: Animator) {}
+        override fun onAnimationRepeat(animation: Animator) {}
+    })
+
+    fab.show()
+    fab.visibility = View.VISIBLE
+
+}
