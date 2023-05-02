0fa40c111af366eb41448c731e7ea9277b980b3d
Ahmed Sheref <ahmedsheef96@gmail.com>
2023-05-02 16:48:56 +0300

    Revert "[handle error for input validation]"
    This reverts commit 28979bedcdc6bd58d3b075224417bedb7526b9bf.

--- Utils/src/main/java/com/el3asas/utils/utils/inputValidation.kt
+++ Utils/src/main/java/com/el3asas/utils/utils/inputValidation.kt
@@ -5,46 +5,17 @@ fun isInputNotValid(
     errorList: List<String>,
     actionsList: List<(String) -> Unit>,
     reverseActionsList: List<() -> Unit>,
-    globalAction: ((String) -> Unit)
-): Boolean {
-    for ((index, item) in conditionsToFail.withIndex()) {
-        if (item) {
-            if (index < actionsList.size) actionsList[index](errorList[index])
-            globalAction(errorList[index])
-            return true
-        } else {
-            if (reverseActionsList.size > index) reverseActionsList[index]()
-        }
-    }
-    return false
-}
-
-
-fun isInputNotValid(
-    conditionsToFail: List<Boolean>,
-    errorList: List<String?>? = null,
-    actionsList: List<(String?) -> Unit>? = null,
-    reverseActionsList: List<() -> Unit>? = null,
-    globalError: String? = null,
     globalAction: ((String) -> Unit)? = null
 ): Boolean {
     for ((index, item) in conditionsToFail.withIndex()) {
         if (item) {
-            if (index < (actionsList?.size ?: -1)) actionsList?.get(index)
-                ?.let { it(errorList?.get(index)) }
-            globalAction?.let { errorAction ->
-                val error = errorList?.get(index)
-
-                if (error != null) {
-                    (errorAction(error))
-                } else if (globalError != null) {
-                    errorAction(globalError)
-                }
-            }
+            if (index < actionsList.size)
+                actionsList[index](errorList[index])
+            globalAction?.let { it(errorList[index]) }
             return true
         } else {
-            if ((reverseActionsList?.size ?: -1) > index) reverseActionsList?.get(index)
-                ?.let { it() }
+            if (reverseActionsList.size > index)
+                reverseActionsList[index]()
         }
     }
     return false
